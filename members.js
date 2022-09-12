451ab14fb1dd9f0793ff2c10ac8a62d3cb3c4cc7
Ayush Peter Junior <ayush_b190921cs@nitc.ac.in>
2022-09-12 16:22:03 +0530

    moved members data to separate file; animations in members page

--- members.js
+++ members.js
@@ -1,58 +1,79 @@
-const slide = () => {
-	const burger = document.querySelector('.burger');
+async function addMembers() {
+    const membsTable = document.querySelector('.membs-table');
+    await fetch("members.json")
+    .then(fileData => {
+        return fileData.json();
+    })
+    .then(jsonData => {
+        let members = jsonData.members;
+        members.forEach((member)=> {
+            let row = document.createElement('tr');
+            let name = document.createElement('td');
+            name.textContent = member.name;
+
+            let rollNo = document.createElement('td');
+            rollNo.textContent = member.rollno;
+
+            let email = document.createElement('td');
+            email.textContent = member.email;
+
+            row.appendChild(name);
+            row.appendChild(rollNo);
+            row.appendChild(email);
+            
+            row.setAttribute('class', 'membs-data');
+            row.setAttribute('cvsrc', member.cv);
+
+            membsTable.appendChild(row);
+        });
+    });
+} 
+
+async function slide() {
+    await addMembers();
+    const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.nav-links-container');
 
 	burger.addEventListener('click', () => {
-		console.log("hi");
+		// console.log("hi");
 		nav.classList.toggle('nav-active');
 	});
-}
 
-let frameON = false;
-
-
-ath_cv = "https://drive.google.com/file/d/1iA4zEE4MunVZDVXkAWa6Xb4axuALifXU/preview"
-apj_cv = "https://drive.google.com/file/d/1iA4zEE4MunVZDVXkAWa6Xb4axuALifXU/preview"
-min_cv = "https://drive.google.com/file/d/1iA4zEE4MunVZDVXkAWa6Xb4axuALifXU/preview"
-sri_cv = "https://drive.google.com/file/d/19y68jDWElziyMT7Us39vaWKcTQJFBsfV/preview"
-
-const frameSlide = () => {
-	const frame = document.querySelector('.side-frame');
-	//frame.setAttribute("src", "");
-	const ath = document.querySelector('.ATH');
-	ath.addEventListener('click', () => {
-		//console.log('hello');
-		frame.classList.add('side-frame-on');
-		frame.setAttribute('src', ath_cv);
-		frameON = true;
-	});
-	const apj = document.querySelector('.APJ');
-	apj.addEventListener('click', () => {
-		//console.log('hello');
-		frame.classList.add('side-frame-on');
-		frame.setAttribute('src', apj_cv);
-		frameON = true;
-	});
-	const min = document.querySelector('.MIN');
-	min.addEventListener('click', () => {
-		//console.log('hello');
-		frame.classList.add('side-frame-on');
-		frame.setAttribute('src', min_cv);
-		frameON = true;
-	});
-	const sri = document.querySelector('.SRI');
-	sri.addEventListener('click', () => {
-		//console.log('hello');
-		frame.classList.add('side-frame-on');
-		frame.setAttribute('src', sri_cv);
-		frameON = true;
-		
-	});
-
-	
-
+    const membsDiv = document.querySelector('.membs');
+    const members = document.querySelectorAll('.membs-data');
+    const cvViewer = document.getElementById('viewer');
+    var selected=-1;
+    console.log(members)
+    members.forEach((member, index) => {
+        member.addEventListener('click', ()=> {
+            cvSrc = member.getAttribute('cvsrc');
+            if (selected == -1) {
+                cvViewer.setAttribute('src', cvSrc);
+    		    cvViewer.classList.toggle('viewer-selected');
+                selected = index;
+            } else if (selected == index) {
+                // cvViewer.setAttribute('src', cvSrc);
+    		    cvViewer.classList.toggle('viewer-selected');
+                selected = -1;
+            } else {
+                cvViewer.setAttribute('src', cvSrc);
+                selected = index;
+            }
+        });
+    });
+    cvViewer.addEventListener('transitionend', ()=> {
+        if (!cvViewer.classList.contains('viewer-selected')) {
+    		membsDiv.classList.toggle('membs-selected');
+            cvViewer.classList.toggle('viewer-hidden');
+        }
+    });
+    cvViewer.addEventListener('transitionstart', ()=> {
+        if (cvViewer.classList.contains('viewer-selected')) {
+            cvViewer.classList.toggle('viewer-hidden');
+    		membsDiv.classList.toggle('membs-selected');
+        }
+    });
 }
 
 
 slide();
-frameSlide();
